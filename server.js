0a107ad72462b731eb2f862159bbfff28c572303
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2025-01-15 14:29:32 +0530

    added cors

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+// Add CORS middleware
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+// Use default middlewares (like logger, etc.)
 server.use(middlewares);
+
+// Use the router
 server.use(router);
 
 const PORT = 3000;
